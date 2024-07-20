c165e95041461fc20bb8a09266bcce4363403675
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-07-20 14:00:44 +0900

    RestTemplate postForEntity 요청

--- src/main/java/com/sparta/springresttemplateclient/service/RestTemplateService.java
+++ src/main/java/com/sparta/springresttemplateclient/service/RestTemplateService.java
@@ -1,6 +1,7 @@
 package com.sparta.springresttemplateclient.service;
 
 import com.sparta.springresttemplateclient.dto.ItemDto;
+import com.sparta.springresttemplateclient.entity.User;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -56,6 +57,7 @@ public class RestTemplateService {
         log.info("uri = " + uri);
 
         // 컬렉션 형태로 데이터를 받을 때는 직렬화되어 있는 String 타입 그대로 받아온다.
+        // 자동으로 json 형태로 변환된다.
         ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri, String.class);
 
         log.info("statusCode = " + responseEntity.getStatusCode());
@@ -78,7 +80,24 @@ public class RestTemplateService {
     }
 
     public ItemDto postCall(String query) {
-        return null;
+        // 요청 URL 만들기
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:7070")
+                .path("/api/server/post-call/{query}")
+                .encode()
+                .build()
+                .expand(query)
+                .toUri();
+        log.info("uri = " + uri);
+
+        User user = new User("Robbie", "1234");
+
+        // 응답 시 자동으로 json 형태로 변환된다.
+        ResponseEntity<ItemDto> responseEntity = restTemplate.postForEntity(uri, user, ItemDto.class);
+
+        log.info("statusCode = " + responseEntity.getStatusCode());
+
+        return responseEntity.getBody();
     }
 
     public List<ItemDto> exchangeCall(String token) {
